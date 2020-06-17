c863e9516aac4c10b523c1a4348fc6044342cc43
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-17 11:24:21 +0000

    Updated files with `git_helper`.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -88,9 +88,6 @@ classifiers = [
 		'Programming Language :: Python :: Implementation :: PyPy',
 		'Programming Language :: Python :: 3 :: Only',
 		'Typing :: Typed',
-		'Operating System :: Microsoft :: Windows',
-		'Operating System :: POSIX :: Linux',
-		'Operating System :: MacOS',
 
 		]
 
