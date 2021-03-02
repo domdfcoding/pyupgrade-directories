5ea90b75e3e56986e6ffa5ef4c53195e012b9212
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-02 17:07:21 +0000

    Bump version v0.1.1 -> v0.2.0

--- pyupgrade_directories/__init__.py
+++ pyupgrade_directories/__init__.py
@@ -16,7 +16,7 @@ from typing import Iterable, List
 __author__ = "Dominic Davis-Foster"
 __copyright__ = "2020 Dominic Davis-Foster"
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __email__ = "dominic@davis-foster.co.uk"
 
 __all__ = ["iter_py_files"]
